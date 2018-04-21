e6f27cfb231a7c1524b32f4e8cabeb1ace52121d
João Victor Germano <37229969+jvictorgermano@users.noreply.github.com>
2018-04-20 22:03:13 -0300

    Desafio 1, Divisão e multiplicação

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-A = input('Digite o valor de A')
-B = input('Digite o valor de B')
-R = (A+B)
-print('O valor de a foi?', A,'O valor de b foi?', B, 'A soma entre os dois é ?')
+A = float(input('Digite o valor de A'))
+B = float(input('Digite o valor de B'))
+R = A*B
+print('O valor de a foi :', A,'O valor de b foi :', B, 'A mutiplicação entre os dois é :', R)
 
